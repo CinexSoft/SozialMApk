ca77af4b775cc5a05a936aa49ab1b783ad0be023
OogleGlu <avirukbasak@gmail.com>
2021-09-10 22:32:16 +0530

    Update files

--- app/src/main/java/chat/app/web/sozialnmedien/MainActivity.java
+++ app/src/main/java/chat/app/web/sozialnmedien/MainActivity.java
@@ -1,30 +1,27 @@
 package chat.app.web.sozialnmedien;
 
 import android.app.Activity;
-import android.app.DownloadManager;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.webkit.DownloadListener;
-import android.webkit.URLUtil;
+import android.content.Context;
+import android.content.Intent;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
-// other classes
 import chat.app.web.sozialnmedien.R;
 import chat.app.web.sozialnmedien.WebAppInterface;
 
 public class MainActivity extends Activity {
     
     private WebView wv;
+    protected static Context context;
     
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+        MainActivity.context = getApplicationContext();
         setContentView(R.layout.main);
+        // requesting permissions
         if (checkSelfPermission("android.permission.READ_EXTERNAL_STORAGE") == -1) {
             requestPermissions(new String[]{"android.permission.READ_EXTERNAL_STORAGE"}, 1000);
         }
@@ -33,6 +30,7 @@ public class MainActivity extends Activity {
         this.wv.getSettings().setJavaScriptEnabled(true);
         this.wv.addJavascriptInterface(new WebAppInterface(this), "Android");
         this.wv.getSettings().setSupportZoom(true);
+        this.wv.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
         this.wv.setScrollbarFadingEnabled(true);
         this.wv.getSettings().setLoadWithOverviewMode(true);
         this.wv.getSettings().setUseWideViewPort(false);
@@ -58,28 +56,6 @@ public class MainActivity extends Activity {
         
         // load webpage
         this.wv.loadUrl("https://sozialnmedien.web.app/chat");
-
-        this.wv.setDownloadListener(new DownloadListener() {
-            @Override
-            public void onDownloadStart(String url, String userAgent,
-                                        String contentDisposition, String mimeType,
-                                        long contentLength) {
-                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-                request.setMimeType(mimeType);
-                request.addRequestHeader("User-Agent", userAgent);
-                request.setDescription("File download started...");
-                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
-                // ignored since API 29
-                request.allowScanningByMediaScanner();
-                // Notify client once download is completed!
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
-                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-                dm.enqueue(request);
-                // To notify the client that the file is being downloaded
-                Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
-            }
-        });
     }
     
     // on back button pressed
@@ -89,10 +65,5 @@ public class MainActivity extends Activity {
         } else {
             finish();
         }
-    }
-    
-    /* Access modifiers changed, original: protected */
-    public void onActivityResult(int i, int i2, Intent intent) {
-        super.onActivityResult(i, i2, intent);
     }
 }
--- app/src/main/java/chat/app/web/sozialnmedien/WebAppInterface.java
+++ app/src/main/java/chat/app/web/sozialnmedien/WebAppInterface.java
@@ -1,15 +1,24 @@
 package chat.app.web.sozialnmedien;
 
-import android.content.Context;
 import android.text.ClipboardManager;
+import android.content.Context;
+import android.app.DownloadManager;
+import android.os.Environment;
+import java.io.IOException;
 import android.webkit.JavascriptInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import java.util.Scanner;
 import android.widget.Toast;
+import android.net.Uri;
+import java.net.URL;
 
 public class WebAppInterface {
-    Context mContext;
+    
+    Context context;
     
     public WebAppInterface(Context c) {
-        mContext = c;
+        context = c;
     }
     
     @JavascriptInterface
@@ -18,13 +27,57 @@ public class WebAppInterface {
     }
     
     @JavascriptInterface
+    public String updateAvailable() {
+        int verCode = 0;
+        try {
+            PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            verCode = pInfo.versionCode;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "failed";
+        }
+        int currentVerCode = 0;
+        try {
+            URL url = new URL("https://sozialnmedien.web.app/records/currentapkversion");
+            Scanner sc = new Scanner(url.openStream());
+            currentVerCode = sc.nextInt();
+            sc.close();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+            return "failed";
+        }
+        return currentVerCode > verCode ? "true" : "false";
+    }
+    
+    @JavascriptInterface
+    public void download(String url, String filename) {
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url)); 
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI |
+                                       DownloadManager.Request.NETWORK_MOBILE);
+        // set title and description
+        request.setTitle("Download");
+        request.setDescription("Downloading " + filename + " from " + url);
+        // ignored by API 29, deprecated before 29
+        request.allowScanningByMediaScanner();
+        // on download complete notification
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        // set the local destination for download file to a path within the application's external files directory
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
+        request.setMimeType("*/*");
+        downloadManager.enqueue(request);
+    }
+    
+    @JavascriptInterface
     public void copyToClipboard(String text) {
-        ClipboardManager clipboard = (ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
         clipboard.setText(text);
     }
     
     @JavascriptInterface
     public void showToast(String toast) {
-        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, toast, Toast.LENGTH_SHORT).show();
     }
 }
